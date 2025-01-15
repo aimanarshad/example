f9dfa241962b61f4bab6c78555afaf671e79a764
Aiman Arshad <aimanat2022@gmail.com>
2025-01-15 08:17:15 +0500

    main

---
+++ sanityClient.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.client = void 0;
+// sanityClient.ts
+const client_1 = require("@sanity/client");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.client = (0, client_1.createClient)({
+    projectId: process.env.projectId,
+    dataset: 'production',
+    apiVersion: '2024-01-04',
+    useCdn: false,
+    token: process.env.token,
+});
